2ceefac701c8a722119a355469dcd737cdeb36da
Soubhagya <taraisoubhagyaranjan@gmail.com>
2023-12-15 16:21:17 +0530

    cfyghvjbnkl

--- one.js
+++ one.js
@@ -1,3 +1,13 @@
+import React, { Component } from 'react'
 function name(params) {
     
+    export class one extends Component {
+      render() {
+        return (
+          <div>one</div>
+        )
+      }
+    }
+    
+    export default one
 }
