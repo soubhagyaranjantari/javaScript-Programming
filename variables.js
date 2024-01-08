e193fbe396e1bd2b89590fa027d015e72ecdba8c
Soubhagya <taraisoubhagyaranjan@gmail.com>
2024-01-08 12:31:10 +0530

    ok

--- variables.js
+++ variables.js
@@ -1,11 +1,14 @@
 var variable=100;
 (
     ()=>{
-        console.log('first',variable);
+        console.log('first',variable);{
+            let variable=10
+            console.log(variable);
+        }
         variable=50
         console.log('sec',variable);
     }
 )()
 console.log('third',variable);
-variable=25
+ variable=25
 console.log('four',variable);
