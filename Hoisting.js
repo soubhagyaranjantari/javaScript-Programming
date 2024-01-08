59ba736b27c7e2860f9562e896ee05341f2b2815
Soubhagya <taraisoubhagyaranjan@gmail.com>
2024-01-08 12:08:24 +0530

    TDZ

--- Hoisting.js
+++ Hoisting.js
@@ -1,3 +1,8 @@
 console.log('the value of A',a);
 var a=10;
 console.log('the value of A',a);
+// thempral dead zone : Accesing the variables which decalre with let and const keyword
+b=100
+console.log('value of b',b);
+var b
+let b;
