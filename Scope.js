6d0ca89784ba1d55719aeca96909f069b4e90ed0
Soubhagya <taraisoubhagyaranjan@gmail.com>
2024-01-08 12:49:35 +0530

    closure

---
+++ Scope.js
@@ -0,0 +1,13 @@
+ var kanhu=100;
+(
+    ()=>{
+        anil=50
+        console.log('first',kanhu);
+        var anil =25
+        kanhu=1000
+        console.log('sec',kanhu);
+    }
+)()
+console.log(anil);
+console.log(kanhu);
+var kanhu =10
